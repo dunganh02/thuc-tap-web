d300a4e4229aa6aed98903ba9e1a68c7e0e0e72f
anhdung02 <padungg8@gmail.com>
2023-04-28 00:04:22 +0700

    da xoa nhieu file

--- pages/main/timkiem.php
+++ pages/main/timkiem.php
@@ -1,64 +1,66 @@
 <?php
-    
 
-	if(isset($_POST['timkiem'])){
-		$tukhoa = $_POST['tukhoa'];
-        
-	}
-	$sql_pro = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc AND tbl_sanpham.tensanpham LIKE '%".$tukhoa."%'  ";
-	$query_pro = mysqli_query($mysqli,$sql_pro);
-	
+
+if (isset($_POST['timkiem'])) {
+    $tukhoa = $_POST['tukhoa'];
+}
+$sql_pro = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc AND tbl_sanpham.tensanpham LIKE '%" . $tukhoa . "%'  ";
+$query_pro = mysqli_query($mysqli, $sql_pro);
+
 ?>
 <div class="headline">
-        <h3>Tìm kiếm : <?php echo $_POST['tukhoa']; ?></h3>
+    <h3>Tìm kiếm : <?php echo $_POST['tukhoa']; ?></h3>
 </div>
 <div class="maincontent">
-            <?php
-                $giaspkm=0;
-                while($row_pro = mysqli_fetch_array($query_pro)){
-                    if ($row_pro['km']>0){$giaspkm=$row_pro['giasp']-($row_pro['giasp']*($row_pro['km']/100));};
-            ?>
-            
-                <ul>
-                <div class="maincontent-item">
-                        <div class="maincontent-top">
+    <?php
+    $giaspkm = 0;
+    while ($row_pro = mysqli_fetch_array($query_pro)) {
+        if ($row_pro['km'] > 0) {
+            $giaspkm = $row_pro['giasp'] - ($row_pro['giasp'] * ($row_pro['km'] / 100));
+        };
+    ?>
 
-                            <?php 
-                                if($row_pro['km']==0){
+        <ul>
+            <div class="maincontent-item">
+                <div class="maincontent-top">
 
-                                }else{
-                            ?>
-                                    <div class="khuyenmai"><?php echo number_format($row_pro['km']).'%' ?></div>
-                            <?php  
-                                }
-                            ?>
-                            <div class="maiconten-top1">
-                                
-                                <a href="index.php?quanly=chitiet&idsanpham=<?php echo $row_pro['id_sanpham'] ?>" class="maincontent-img">
-                                    <img src="./admincp/modules/quanlysp/uploads/<?php echo $row_pro['hinhanh'] ?>">
-                                </a>
-                                <button type  ="submit" title = 'chi tiet' class="muangay"  name="chitiet"><a href="index.php?quanly=chitiet&idsanpham=<?php echo $row_pro['id_sanpham'] ?>">Chi tiết</a></button>
-                                <form method="POST" action="./pages/main/themgiohang.php?idsanpham=<?php echo $row_pro['id_sanpham'] ?>">
-                                <button type  = "submit" title = 'thêm vào giỏ' name="themgiohang" class="giohang"><a >thêm vào giỏ</a></button>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="maincontent-info">
-                            <a href="index.php?quanly=sanpham&id=<?php echo $row_pro['id_sanpham'] ?>" class="maincontent-name"><?php echo $row_pro['tensanpham'] ?></a>
-                            <a href="index.php?quanly=sanpham&id=<?php echo $row_pro['id_sanpham'] ?>" class="maincontent-gia"><?php if($row_pro['km']>0){ echo number_format($giaspkm).'vnd'; }else {echo number_format($row_pro['giasp']).'vnd';} ?>
-                                            <span><?php if($row_pro['km']>0){
-                                                    echo number_format($row_pro['giasp']).'vnd';
-                                                    }else{
+                    <?php
+                    if ($row_pro['km'] == 0) {
+                    } else {
+                    ?>
+                        <div class="khuyenmai"><?php echo number_format($row_pro['km']) . '%' ?></div>
+                    <?php
+                    }
+                    ?>
+                    <div class="maiconten-top1">
 
-                                                    }
-                                                    ?>                                                                                                                                                                                                        
-                                            </span></a>
-                        </div>
+                        <a href="index.php?quanly=chitiet&idsanpham=<?php echo $row_pro['id_sanpham'] ?>" class="maincontent-img">
+                            <img src="./admincp/modules/quanlysp/uploads/<?php echo $row_pro['hinhanh'] ?>">
+                        </a>
+                        <button type="submit" title='chi tiet' class="muangay" name="chitiet"><a href="index.php?quanly=chitiet&idsanpham=<?php echo $row_pro['id_sanpham'] ?>">Chi tiết</a></button>
+                        <form method="POST" action="./pages/main/themgiohang.php?idsanpham=<?php echo $row_pro['id_sanpham'] ?>">
+                            <button type="submit" title='thêm vào giỏ' name="themgiohang" class="giohang"><a>thêm vào giỏ</a></button>
+                        </form>
                     </div>
-                </ul>
-            <?php
-                }
-            ?>
-            
-        </div>
-        
+                </div>
+                <div class="maincontent-info">
+                    <a href="index.php?quanly=sanpham&id=<?php echo $row_pro['id_sanpham'] ?>" class="maincontent-name"><?php echo $row_pro['tensanpham'] ?></a>
+                    <a href="index.php?quanly=sanpham&id=<?php echo $row_pro['id_sanpham'] ?>" class="maincontent-gia"><?php if ($row_pro['km'] > 0) {
+                                                                                                                            echo number_format($giaspkm) . 'vnd';
+                                                                                                                        } else {
+                                                                                                                            echo number_format($row_pro['giasp']) . 'vnd';
+                                                                                                                        } ?>
+                        <span><?php if ($row_pro['km'] > 0) {
+                                    echo number_format($row_pro['giasp']) . 'vnd';
+                                } else {
+                                }
+                                ?>
+                        </span></a>
+                </div>
+            </div>
+        </ul>
+    <?php
+    }
+    ?>
+
+</div>
